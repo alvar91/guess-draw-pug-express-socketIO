a312d5fcf6676a604b3bb4a7e704f23443c431ff
Aleksey <aleksey91scorp@bk.ru>
2020-06-18 22:18:51 +0300

    the client said hello

--- src/static/index.js
+++ src/static/index.js
@@ -1,2 +1,6 @@
-const hello = "hello";
-hello;
+// eslint-disable-next-line no-undef
+const socket = io("/");
+
+socket.on("hello", () => console.log("Somebody joined"));
+
+setTimeout(() => socket.emit("helloGuys"), 4000);
